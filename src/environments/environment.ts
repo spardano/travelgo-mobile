e91942e4b776079c6209b18731a8a2a78b8c09d8
sakti pardano <Sakti Pardano>
2023-07-09 19:57:08 +0700

    payment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
 
   //ngrok
-  base_url: 'https://c3fb-114-10-84-188.ngrok-free.app',
-  base_api: 'https://c3fb-114-10-84-188.ngrok-free.app/api',
+  base_url: 'https://d8cd-114-5-145-76.ngrok-free.app',
+  base_api: 'https://d8cd-114-5-145-76.ngrok-free.app/api',
   //travel go webservice
   // base_api: 'http://172.25.208.1:8000/api',
   // base_api: 'http://web-travelgo.test/api',
